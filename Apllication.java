366bd7e48e1db7e4febfae0cc4d4aac1ea1bc812
durgamkarna <durgamkarna1996@gmail.com>
2023-06-14 17:40:28 +0530

    modified Application.java by KARNA

--- Apllication.java
+++ Apllication.java
@@ -15,3 +15,22 @@ public class EvenOdd {
             System.out.println(num + " is odd");
     }
 }
+
+#feature201 changed by Karna on 14 june 20223
+ import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        String evenOdd = (num % 2 == 0) ? "even" : "odd";
+
+        System.out.println(num + " is " + evenOdd);
+
+    }
+}
